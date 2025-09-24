d987003dd13209992de19bb4d6a5da65c9f8f0a8
Matheus Mathias <mathias.matheus76@gmail.com>
2025-09-24 18:52:45 -0300

    post de users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fast_zero.schemas import Message, UserSchema
+from http import HTTPStatus
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá Mundo!"}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+    ...
