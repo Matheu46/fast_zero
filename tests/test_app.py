8d2aeba721ef13c1e9298ad3fe59c45281b31238
Matheus Mathias <mathias.matheus@ymail.com>
2025-09-24 22:53:49 -0300

    finalização post users e testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,28 @@
-from http import HTTPStatus  
-
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
+from http import HTTPStatus
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():  
-    client = TestClient(app) # Arrange
+def test_root_deve_retornar_ok_e_ola_mundo(client):
+    # Arrange
 
-    response = client.get('/')  # Act
+    response = client.get("/")  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+    assert response.json() == {"message": "Olá Mundo!"}  # Assert
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
